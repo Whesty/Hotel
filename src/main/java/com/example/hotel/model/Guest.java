c27ef5c81b15f7d848e9850a1961af87f5452b05
Диана <83098361+Whesty@users.noreply.github.com>
2022-11-01 22:14:43 +0300

    Initial commit

---
+++ src/main/java/com/example/hotel/model/Guest.java
@@ -0,0 +1,14 @@
+package com.example.hotel.model;
+
+import java.util.Date;
+
+//Обьект Гость
+public class Guest {
+    int Id;
+    String Lastname; //Фамилия
+    String Firstname; //Имя
+    String Secendname; //Отчество
+    String email;
+    Date Birthday;
+
+}
