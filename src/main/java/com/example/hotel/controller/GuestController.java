6b9f9b633db38c4f074db692c5d8fde2b87aecbe
Диана <83098361+Whesty@users.noreply.github.com>
2022-11-02 11:39:43 +0300

    Create CreateGuest.html (Errors)

--- src/main/java/com/example/hotel/controller/GuestController.java
+++ src/main/java/com/example/hotel/controller/GuestController.java
@@ -3,13 +3,17 @@ package com.example.hotel.controller;
  * Аннотация @RequestMapping используется для связывания с URL для всего класса
  * */
 
+import com.example.hotel.forms.GuestForm;
 import com.example.hotel.model.Guest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Slf4j
@@ -17,5 +21,36 @@ import java.util.List;
 @RequestMapping
 public class GuestController {
     private static List<Guest> guests = new ArrayList<Guest>();
-
+@GetMapping(value = {"/CreateGuest"})
+    public ModelAndView SaveGuest(Model model){
+    ModelAndView modelAndView = new ModelAndView("CreateGuest");
+    GuestForm guestForm = new GuestForm();
+    model.addAttribute("guestform", guestForm);
+    log.info("/CreateGuest was called");
+    return modelAndView;
+}
+@PostMapping(value = {"/CreateGuest"})
+    public ModelAndView SaveGuest(Model model, @ModelAttribute("guestform") GuestForm guestForm){
+    ModelAndView modelAndView = new ModelAndView();
+    modelAndView.setViewName("ReserveRoom");
+    int id = guests.size();
+    String Lastname = guestForm.getLastname();
+    String Firstname = guestForm.getFirstname();
+    String Secendname = guestForm.getSecendname();
+    String email = guestForm.getEmail();
+    Date birthday = guestForm.getBirthday();
+    if (Lastname != null && Lastname.length() > 0 //
+            && Firstname != null && Firstname.length() > 0 //
+            && Secendname != null && Secendname.length() > 0 //
+            && email != null && email.length() > 0) {
+        Guest newGuest = new Guest(id, Lastname, Firstname, Secendname, email, birthday);
+        guests.add(newGuest);
+        model.addAttribute("guests", guests);
+        return modelAndView;
+    }
+    model.addAttribute("errorMessage", "Error");
+    modelAndView.setViewName("CreateGuest");
+    log.info("/CreateGuest was called");
+    return modelAndView;
+}
 }
--- src/main/java/com/example/hotel/model/Guest.java
+++ src/main/java/com/example/hotel/model/Guest.java
@@ -1,8 +1,15 @@
 package com.example.hotel.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Date;
 
 //Обьект Гость
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Guest {
     private int Id;
     private String Lastname; //Фамилия
@@ -10,5 +17,6 @@ public class Guest {
     private String Secendname; //Отчество
     private String email; //Почта
     private Date Birthday; //Дата рождения
+
 
 }
